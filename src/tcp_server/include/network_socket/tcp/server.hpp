52773637aea4a00b3b0009d7fab7ac8af92a0568
Gabriel Sales Lins Rodrigues <gabriellins2@gmail.com>
2020-05-23 03:02:36 -0300

    Implement tcp::Server

--- src/tcp_server/include/network_socket/tcp/server.hpp
+++ src/tcp_server/include/network_socket/tcp/server.hpp
@@ -1,1 +1,30 @@
-// network_socket/tcp/server.hpp
+#ifndef NETSOCK_TCP_SERVER_HPP
+#define NETSOCK_TCP_SERVER_HPP
+
+// 
+#include "network_socket/tcp/socket.hpp"
+
+namespace network_socket
+{
+	namespace tcp
+	{
+		class Server : public Socket
+		{
+		public:
+			Server(const unsigned int &t_port);
+			virtual ~Server(void);
+
+			TcpOperationStatus acceptConnection(const uint16_t &t_timeoutLimit = DEFAULT_TIMEOUT_LIMIT);
+			void setPort(const unsigned int &t_port);
+
+		private:
+			void acceptHandler(const boost::system::error_code &t_errorCode);
+			void checkDeadline(void) override;
+
+			boost::asio::ip::tcp::acceptor m_acceptor;
+			bool m_ongoingAccept;
+		};
+	}
+}
+
+#endif // NETSOCK_TCP_SERVER_HPP
--- src/tcp_server/src/server.cpp
+++ src/tcp_server/src/server.cpp
@@ -1,1 +1,88 @@
 #include "network_socket/tcp/server.hpp"
+
+namespace network_socket
+{
+	namespace tcp
+	{
+		Server::Server(const unsigned int &t_port) : m_acceptor(m_ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), t_port)), m_ongoingAccept(false)
+		{
+			// 
+			checkDeadline();
+		}
+
+		Server::~Server(void)
+		{
+			disconnect();
+		}
+
+		TcpOperationStatus Server::acceptConnection(const uint16_t &t_timeoutLimit)
+		{
+			try
+			{
+				if (!m_isSocketConnected)
+				{
+					// 
+					m_deadlineTimer.expires_from_now(boost::posix_time::seconds(t_timeoutLimit));
+
+					// 
+					m_ongoingAccept = true;
+					m_acceptor.async_accept(m_socket, boost::bind(&Server::acceptHandler, this, boost::asio::placeholders::error));
+
+					// 
+					do
+					{
+						m_ioService.run_one();
+					} while (m_ongoingAccept);
+
+					// 
+					m_isSocketConnected = true;
+					return TcpOperationStatus{true, ""};
+				}
+				else
+				{
+					return TcpOperationStatus{false, "Socket already connected"};
+				}
+			}
+			catch (std::exception &e)
+			{
+				return TcpOperationStatus{false, e.what()};
+			}
+		}
+
+		void Server::setPort(const unsigned int &t_port)
+		{
+			disconnect();
+			m_acceptor.close();
+			m_acceptor = boost::asio::ip::tcp::acceptor(m_ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), t_port));
+		}
+
+		void Server::acceptHandler(const boost::system::error_code &t_errorCode)
+		{
+			// 
+			if (t_errorCode || !m_socket.is_open())
+			{
+				throw boost::system::system_error(t_errorCode ? t_errorCode : boost::asio::error::operation_aborted);
+			}
+
+			m_ongoingAccept = false;
+		}
+
+		void Server::checkDeadline(void)
+		{
+			// 
+			if (m_deadlineTimer.expires_at() <= boost::asio::deadline_timer::traits_type::now())
+			{
+				// 
+				boost::system::error_code ignoredErrorCode;
+				m_socket.close(ignoredErrorCode);
+				m_acceptor.cancel(ignoredErrorCode);
+
+				// 
+				m_deadlineTimer.expires_at(boost::posix_time::pos_infin);
+			}
+
+			// 
+			m_deadlineTimer.async_wait(boost::lambda::bind(&Server::checkDeadline, this));
+		}
+	}
+}
