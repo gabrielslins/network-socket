51422f6b9a2b5977b5c6e1ed6c733deccd4d0e75
Gabriel Sales Lins Rodrigues <gabriellins2@gmail.com>
2020-05-23 03:02:11 -0300

    Implement tcp::Client

--- src/tcp_client/include/network_socket/tcp/client.hpp
+++ src/tcp_client/include/network_socket/tcp/client.hpp
@@ -1,1 +1,25 @@
-// network_socket/tcp/client.hpp
+#ifndef NETSOCK_TCP_CLIENT_HPP
+#define NETSOCK_TCP_CLIENT_HPP
+
+// 
+#include "network_socket/tcp/socket.hpp"
+
+namespace network_socket
+{
+	namespace tcp
+	{
+		class Client : public Socket
+		{
+		public:
+			Client(void);
+			virtual ~Client(void);
+
+			TcpOperationStatus connect(const std::string &t_host, const std::string &t_port, const uint16_t &t_timeoutLimit = DEFAULT_TIMEOUT_LIMIT);
+
+		private:
+			void checkDeadline(void) override;
+		};
+	}
+}
+
+#endif // NETSOCK_TCP_CLIENT_HPP
--- src/tcp_client/src/client.cpp
+++ src/tcp_client/src/client.cpp
@@ -1,1 +1,81 @@
 #include "network_socket/tcp/client.hpp"
+
+namespace network_socket
+{
+	namespace tcp
+	{
+		Client::Client(void)
+		{
+			// 
+			checkDeadline();
+		}
+
+		Client::~Client(void)
+		{
+			disconnect();
+		}
+
+		TcpOperationStatus Client::connect(const std::string &t_host, const std::string &t_port, const uint16_t &t_timeoutLimit)
+		{
+			try
+			{
+				if (!m_isSocketConnected)
+				{
+					// 
+					boost::asio::ip::tcp::resolver::query query(t_host, t_port);
+					boost::asio::ip::tcp::resolver::iterator iter = boost::asio::ip::tcp::resolver(m_ioService).resolve(query);
+
+					// 
+					m_deadlineTimer.expires_from_now(boost::posix_time::seconds(t_timeoutLimit));
+
+					// 
+					boost::system::error_code errorCode = boost::asio::error::would_block;
+
+					// 
+					boost::asio::async_connect(m_socket, iter, boost::lambda::var(errorCode) = boost::lambda::_1);
+
+					// 
+					do
+					{
+						m_ioService.run_one();
+					} while (errorCode == boost::asio::error::would_block);
+
+					// 
+					if (errorCode || !m_socket.is_open())
+					{
+						throw boost::system::system_error(errorCode ? errorCode : boost::asio::error::operation_aborted);
+					}
+
+					// 
+					m_isSocketConnected = true;
+					return TcpOperationStatus{true, ""};
+				}
+				else
+				{
+					return TcpOperationStatus{false, "Socket already connected"};
+				}
+			}
+			catch (std::exception &e)
+			{
+				return TcpOperationStatus{false, e.what()};
+			}
+		}
+
+		void Client::checkDeadline(void)
+		{
+			// 
+			if (m_deadlineTimer.expires_at() <= boost::asio::deadline_timer::traits_type::now())
+			{
+				// 
+				boost::system::error_code ignoredErrorCode;
+				m_socket.close(ignoredErrorCode);
+
+				// 
+				m_deadlineTimer.expires_at(boost::posix_time::pos_infin);
+			}
+
+			// 
+			m_deadlineTimer.async_wait(boost::lambda::bind(&Client::checkDeadline, this));
+		}
+	}
+}
