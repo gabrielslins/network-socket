ec8830a3257b4883e4c38e7c64a334c18830e2ca
Gabriel Sales Lins Rodrigues <gabriellins2@gmail.com>
2020-05-24 23:55:04 -0300

    Set up precompiled headers

--- include/network_socket/tcp/socket.hpp
+++ include/network_socket/tcp/socket.hpp
@@ -1,24 +1,8 @@
 #ifndef NETSOCK_TCP_SOCKET_HPP
 #define NETSOCK_TCP_SOCKET_HPP
 
-// Include standard C++ libraries
-#include <cstdlib>
-#include <string>
-
-// Include third-party libraries
-#include <boost/asio/connect.hpp>
-#include <boost/asio/deadline_timer.hpp>
-#include <boost/asio/io_service.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/placeholders.hpp>
-#include <boost/asio/read.hpp>
-#include <boost/asio/read_until.hpp>
-#include <boost/asio/streambuf.hpp>
-#include <boost/asio/write.hpp>
-#include <boost/bind.hpp>
-#include <boost/lambda/bind.hpp>
-#include <boost/lambda/lambda.hpp>
-#include <boost/system/system_error.hpp>
+// Include pre-compiled headers
+#include "network_socket/tcp/pch.hpp"
 
 // Macros
 #define DEFAULT_TIMEOUT_LIMIT		30
